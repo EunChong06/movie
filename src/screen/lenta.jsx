e1fc68b859e94f3d319947d95f54940f00ec5c7d
YaphetS <qwer_lala@mail.ru>
2023-05-05 18:14:51 +0600

    a

--- src/screen/lenta.jsx
+++ src/screen/lenta.jsx
@@ -40,27 +40,26 @@ const Lenta = () => {
                     <div className='col-md-2 my-2'>
                         <a href='/'><img src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg' /></a>
                     </div>
-                    <div className='col-md-1 my-2 '><a href="/popular">Фильмы</a></div>
-                    <div className='col-md-1 my-2 '><a href="/tv">Сериалы</a></div>
-                    <div className='col-md-1 my-2 '>Люди</div>
-                    <div className='col-md-1 my-2 '>еще</div>
-                    <div className='col-md-2 my-2'></div>
-                    <div className='col-md-1 my-2 '><b>+</b></div>
-                    <div className='col-md-1 my-2 '>RU</div>
-                    <div className='col-md-2 my-2'>Присоединиться к TMDB</div>
+                    <div className='col-4 my-2 '><a href="/popular">Фильмы</a></div>
+                    <div className='col-4 my-2 '><a href="/tv">Сериалы</a></div>
+                    <div className='col-4 my-2 '>Люди</div>
+                    <div className='col-4 my-2 '>еще</div>
+                    <div className='col-4 my-2 '><b>+</b></div>
+                    <div className='col-4 my-2 '>RU</div>
+                    <div className='col-12 my-2'>Присоединиться к TMDB</div>
                 </div>
-                <div className='row' style={{ backgroundImage: "url(https://www.themoviedb.org/t/p/w355_and_h200_multi_faces/o8zk3QmHYMSC7UiJgFk81OFF1sc.jpg)", height: "140px" }}>
-                    <div className='col-md-12 text-light'>
-                        <h1>
+                <div className='row' style={{ backgroundImage: "url(https://avatars.mds.yandex.net/i?id=de35505d592ebf874118fb4360957b55c7335c3c-4253964-images-thumbs&n=13)", height: "140px" }}>
+                    <div className='col-md-12 text-light text-center'>
+                        <b>
                             Добропожаловать
                             <br />
-                        </h1>
-                        <h2>Миллионы фильмов, сериалов и людей. Исследуйте сейчас.</h2>
+                        </b>
+                        <p>Миллионы фильмов, сериалов и людей. Исследуйте сейчас.</p                                                                                                                                                                            >
                     </div>
-                    <div className='col-md-9'>
+                    <div className='col-8 mt-5'>
                         <input placeholder='найти фильмь, сериал...' className='form-control' onChange={(e) => setSearch(e.target.value)} onInput={Search} />
                     </div>
-                    <div className='col-md-3'>
+                    <div className='col-4 mt-5'>
                         <button className='form-control' onClick={Search}>Поиск</button>
                     </div>
                     <div className='row mt-3'>
@@ -73,11 +72,9 @@ const Lenta = () => {
                                         </div>
                                     )
                                     }
-
                                 </>
                                 :
                                 <>
-
                                 </>
                             }
                             </div>
@@ -90,9 +87,9 @@ const Lenta = () => {
                                 {movies.length > 0 ?
                                     <>
                                         {movies.map((item) =>
-                                            <div className='col-md-2 px-3'>
+                                            <div className='col-6 px-3'>
                                                 <img src={'https://www.themoviedb.org/t/p/w220_and_h330_face' + item.backdrop_path} width="100%" />
-                                                <a href={'/movie/' + item.id}><b>{item.title}</b></a>
+                                                <small><a href={'/movie/' + item.id}><b>{item.title}</b></a></small>
                                                 <br />
                                                 <i>{moment(item.release_date, "YYYYMMDD").fromNow()}</i>
                                             </div>
--- src/screen/movie.jsx
+++ src/screen/movie.jsx
@@ -34,34 +34,31 @@ const Movie = () => {
     return (
         <div>
             <div className='container-fluid'>
-                <div className='row text-center bg-dark text-light'>
+            <div className='row text-center bg-dark text-light'>
                     <div className='col-md-2 my-2'>
                         <a href='/'><img src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg' /></a>
                     </div>
-                    <div className='col-md-1 my-2 '><a href="/popular">Фильмы</a></div>
-                    <div className='col-md-1 my-2 '><a href="/tv">Сериалы</a></div>
-                    <div className='col-md-1 my-2 '>Люди</div>
-                    <div className='col-md-1 my-2 '>еще</div>
-                    <div className='col-md-2 my-2'></div>
-                    <div className='col-md-1 my-2 '><b>+</b></div>
-                    <div className='col-md-1 my-2 '>RU</div>
-                    <div className='col-md-2 my-2'>Присоединиться к TMDB</div>
+                    <div className='col-4 my-2 '><a href="/popular">Фильмы</a></div>
+                    <div className='col-4 my-2 '><a href="/tv">Сериалы</a></div>
+                    <div className='col-4 my-2 '>Люди</div>
+                    <div className='col-4 my-2 '>еще</div>
+                    <div className='col-4 my-2 '><b>+</b></div>
+                    <div className='col-4 my-2 '>RU</div>
+                    <div className='col-12 my-2'>Присоединиться к TMDB</div>
                 </div>
-                <div className="row">
-                    <div className="col-md-4"></div>
-                    <div className="col-md-1">Обзор</div>
-                    <div className="col-md-1">Медиа</div>
-                    <div className="col-md-1">Фандом</div>
-                    <div className="col-md-1">Поделиться</div>
-                    <div className="col-md-4"></div>
+                <div className="row text-center">
+                    <div className="col-6">Обзор</div>
+                    <div className="col-6">Медиа</div>
+                    <div className="col-6 mt-2">Фандом</div>
+                    <div className="col-6 mt-w">Поделиться</div>
                 </div>
                 {details != null ?
-                    <div className="row" style={{ backgroundImage: "url(https://www.themoviedb.org/t/p/w533_and_h300_bestv2" + details.poster_path+")", height: "510px", backgroundSize: "cover" }}>
+                    <div className="row" style={{ backgroundImage: "url(https://www.themoviedb.org/t/p/w533_and_h300_bestv2" + details.poster_path+")", height: "530px", backgroundSize: "cover" }}>
 
 
                         <div className="col-md-3 mt-5 ">
                             {details != null ?
-                                <img src={"https://www.themoviedb.org/t/p/w300_and_h450_bestv2" + details.backdrop_path} style={{ borderRadius: "5px" }}></img>
+                                <img src={"https://www.themoviedb.org/t/p/w300_and_h450_bestv2" + details.backdrop_path} style={{ borderRadius: "5px" }} width={"100%"} height={300}></img>
                                 : <></>
                             }
                         </div>
@@ -89,7 +86,7 @@ const Movie = () => {
                     </div>
                     : <></>
                 }
-                <div className="row">
+                <div className="row mt-5">
                 {videos.length > 0 ?
                 <>
                 {videos.map((item)=>
